4df36a9ca422e508f65b8e523c06742709187c54
kevin lee <klee041185@gmail.com>
2024-01-12 10:42:29 +0000

    yaml update

--- test_data_sets.py
+++ test_data_sets.py
@@ -3,16 +3,17 @@ import pytest
 
 
 data_set = [
-    (1, 'Leanne Graham', 'Gwenborough'),
-    (2, 'Ervin Howell', 'Wisokyburgh'),
-    (3, 'Clementine Bauch', 'McKenziehaven'),
-    (4, 'Patricia Lebsack', 'South Elvis')
+    (1, 'Leanne Graham', 'Gwenborough', '-37.3159'),
+    (2, 'Ervin Howell', 'Wisokyburgh', '-43.9509'),
+    (3, 'Clementine Bauch', 'McKenziehaven', '-68.6102'),
+    (4, 'Patricia Lebsack', 'South Elvis', '29.4572')
     ]
 
 
-@pytest.mark.parametrize('id, name, address_city', data_set)
-def test_for_names(id, name, address_city):
+@pytest.mark.parametrize('id, name, address_city, latitude, data_set)
+def test_for_names(id, name, address_city, latitude):
     response = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
     body = response.json()
     assert body['name'] == name
     assert body ['address']['city'] == address_city
+    assert body['address']['geo']['lat'] == latitude
