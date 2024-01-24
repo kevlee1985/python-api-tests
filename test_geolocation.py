4f479f86a5e42cadbaed81d96c6bce5709801f29
kevin lee <klee041185@gmail.com>
2024-01-24 14:53:26 +0000

    geo commit

--- test_geolocation.py
+++ test_geolocation.py
@@ -5,7 +5,7 @@ import variables
 url = variables.base_url
 
 
-def test_get():
+def test_tc00():
     response = requests.post(url)
     print(f'Status code is: {response.status_code}')
     assert response.status_code == 400
@@ -27,3 +27,12 @@ def test_tc02():
     json_data = response.json()
     assert json_data["locationOk"] == False
     assert response.status_code == 200
+
+
+def test_tc03():
+    headers = {"Cookie": variables.Cookie}
+    data = json.load(open('data/TC03.json', 'r'))
+    response = requests.post(url, json=data, headers=headers)
+    json_data = response.json()
+    assert json_data["error"] == "Bad Request"
+    assert response.status_code == 400
