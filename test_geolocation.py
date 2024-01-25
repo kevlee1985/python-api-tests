f588d947ff74406a3e8c3a7dfdb5ce984b28915e
kevin lee <klee041185@gmail.com>
2024-01-25 12:14:14 +0000

    geo commit

--- test_geolocation.py
+++ test_geolocation.py
@@ -5,6 +5,13 @@ import variables
 url = variables.base_url
 
 
+def post_call(file_name: str):
+    headers = {"Cookie": variables.Cookie}
+    data = json.load(open('data/' + file_name + '.json', 'r'))
+    response = requests.post(url, json=data, headers=headers)
+    return response
+
+
 # TC00 - invalid get request to URL
 def test_tc00():
     response = requests.post(url)
@@ -14,29 +21,23 @@ def test_tc00():
 
 # TC01 - UK Based IP/Coordinates request
 def test_tc01():
-    headers = {"Cookie": variables.Cookie}
-    data = json.load(open('data/TC01.json', 'r'))
-    response = requests.post(url, json=data, headers=headers)
-    json_data = response.json()
+    response = post_call("TC01")
+    json_data = json.loads(response.content)
     assert json_data["locationOk"] == True
     assert response.status_code == 200
 
 
 # TC02 - Non UK Based IP/Coordinates request
 def test_tc02():
-    headers = {"Cookie": variables.Cookie}
-    data = json.load(open('data/TC02.json', 'r'))
-    response = requests.post(url, json=data, headers=headers)
-    json_data = response.json()
+    response = post_call("TC02")
+    json_data = json.loads(response.content)
     assert json_data["locationOk"] == False
     assert response.status_code == 200
 
 
 # TC03 - Incorrectly formatted JSON request
 def test_tc03():
-    headers = {"Cookie": variables.Cookie}
-    data = json.load(open('data/TC03.json', 'r'))
-    response = requests.post(url, json=data, headers=headers)
-    json_data = response.json()
+    response = post_call("TC03")
+    json_data = json.loads(response.content)
     assert json_data["error"] == "Bad Request"
     assert response.status_code == 400
